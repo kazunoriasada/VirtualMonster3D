3e218cae5bafc0586fb3f348a9b85488b088387c
浅田和則 <kazu.july05@gmail.com>
2019-11-04 21:06:18 +0900

    Navmesh

---
+++ Assets/Scripts/MoveAndTaik.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndTalk : MonoBehaviour
+{
+    //定数 = 変更しない一定の数
+    //enum = 複数の定数をひとつにまとめておくことができる型
+    public enum CharacterState 
+    {
+        normal,
+        talk
+    }
+
+    private CharacterController characterController;
+    private Animator animator;
+    [SerializeField]
+    private float walkSpeed = 1.25f;
+    private Vector3 velocity;
+    private CharacterState characterState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        characterState = CharacterState.normal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 通常動作
+        if (characterState == CharacterState.normal) {
+            Move();
+        // 会話中
+        } else if(characterState == CharacterState.talk) {
+            Talk();
+        }
+ 
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+     void Move() 
+     {
+        if (characterController.isGrounded) {
+            velocity = Vector3.zero;
+            var input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+ 
+            if (input.magnitude > 0f) {
+                velocity = input.normalized * walkSpeed;
+                animator.SetFloat("WalkSpeed", input.magnitude);
+                transform.LookAt(transform.position + input.normalized);
+            } else {
+                animator.SetFloat("WalkSpeed", 0f);
+            }
+        }
+    }
+
+    //会話中
+    void Talk() 
+    {
+ 
+    }
+
+    //状態確認メソッド
+    public CharacterState GetState() 
+    {
+        return characterState;
+    }
+    //状態変更メソッド
+    public void SetState(CharacterState setState) 
+    {
+        characterState = setState;
+        //会話の最中は動かない
+        if(characterState == CharacterState.talk) 
+        {
+            velocity = Vector3.zero;
+            animator.SetFloat("WalkSpeed", 0f);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/VillagerTalk.cs
@@ -0,0 +1,119 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class VillagerTalk : MonoBehaviour
+// {
+//     //会話のUI
+//     [SerializeField]
+//     private GameObject talkUI;
+//     //主人公キャラクター操作スクリプト
+//     [SerializeField]
+//     private MoveAndTalkChara moveAndTalkChara;
+//     //今見ている文字番号
+//     private int nowTextNum = 0;
+
+//     [SerializeField]
+//     [TextArea(1, 10)]
+//     private string talkMessage = "初期メッセージ";
+
+//     //1回分のメッセージを表示したかどうか
+//     private bool isOneMessage = false;
+//     //メッセージをすべて表示したかどうか
+//     private bool isEndMessage = false;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         messageText.text = "";
+//         SetMessage(talkMessage);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //メッセージが終わっていない、または設定されていない、または会話が開始されていない
+//         if (isEndMessage || message == null || !isTalk) 
+//         {
+//             return;
+//         }
+ 
+//         //1回に表示するメッセージを表示していない	
+//         if (!isOneMessage) 
+//         {
+ 
+//             //その他処理
+ 
+//             //メッセージ表示中にマウスの左ボタンを押したら一括表示
+//             if (Input.GetButtonDown("Action")) 
+//             {
+ 
+//                 //その他処理
+//                 Input.ResetInputAxes();
+//             }
+//         //1回に表示するメッセージを表示した
+//         } 
+//         else
+//         {
+ 
+//             //その他処理
+//             //マウスクリックされたら次の文字表示処理
+//             if (Input.GetButtonDown("Action")) 
+//             {
+ 
+//                 //その他処理
+//                 //メッセージが全部表示されていたら通常
+//                 if (nowTextNum >= message.Length) 
+//                 {
+//                     nowTextNum = 0;
+//                     isEndMessage = true;
+ 
+//                     Debug.Log("通常");
+//                     talkUI.SetActive(false);
+//                     isTalk = false;
+//                     moveAndTalkChara.SetState(MoveAndTalkChara.CharacterState.normal);
+//                 }
+//                 Input.ResetInputAxes();
+//             }
+//         }
+//     }
+//     //現在会話中かどうか
+//     private bool isTalk;
+    
+//     private void OnTriggerStay(Collider other) 
+//     {
+
+//         if(other.tag == "Player") 
+//         {
+//             //他の人と会話している場合は何もしない
+//             if (moveAndTalkChara.GetState() == MoveAndTalkChara.CharacterState.talk) 
+//             {
+//                 return;
+//             }
+//             // Actionに割り当てたキーで会話開始と終了
+//             if (Input.GetButtonDown("Action")) 
+//             {
+//                 if (!isTalk) 
+//                 {
+//                     Debug.Log("会話");
+//                     //会話リセットメソッドを読んで会話を初期化する
+//                     InitializeTalk();
+
+//                     //1フレームに何度もキー押し判定されないようにリセット
+//                     Input.ResetInputAxes();
+//                 }
+//             }
+//         }
+//     }
+//     //会話を初期化する
+//     void InitializeTalk() 
+//     {
+//         isOneMessage = false;
+//         isEndMessage = false;
+//         moveAndTalkChara.SetState(MoveAndTalkChara.CharacterState.talk);
+//         talkUI.SetActive(true);
+//         isTalk = true;
+//     }
+
+    
+// }
