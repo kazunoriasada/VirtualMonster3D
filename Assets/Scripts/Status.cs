8b017d68ae97f35acfa9b5f2eb10e87b7fbdf0f5
浅田和則 <kazu.july05@gmail.com>
2019-11-07 11:02:02 +0900

    Status

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public static int Lv_p = 1;
+    public static float HP = 100;
+    public static float HP_max = 100;
+    public static float ATK = 10;
+    public static float EXP_rui = 0;
+    public static float EXP_nextrui = 20;
+    public static float EXP_next = 20;
+ // Use this for initialization
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
